98757fb29a130d6bc7c65bbd211754fc6ff6eab6
Khushim8 <124706002+Khushim8@users.noreply.github.com>
2023-10-03 17:36:22 +0530

    Update pyconn.py

--- pyconn.py
+++ pyconn.py
@@ -1,26 +1,56 @@
 import mysql.connector
 
-# Establish the connection
-conn = mysql.connector.connect(
-    host='your_host',       # Replace with your MySQL host
-    user='your_username',   # Replace with your MySQL username
-    password='your_password',  # Replace with your MySQL password
-    database='your_database'  # Replace with your MySQL database name
-)
+# Replace these with your actual database credentials
+db_config = {
+    "host": "your_host",
+    "user": "your_username",
+    "password": "your_password",
+    "database": "your_database_name"
+}
 
-# Create a cursor object to execute SQL queries
+# Connect to the database
+conn = mysql.connector.connect(**db_config)
+
+# Create a cursor object to interact with the database
 cursor = conn.cursor()
 
-# Execute a sample query
-cursor.execute("SELECT * FROM Main")
+# Prompt the user for input
+from_city = input("Enter the departure city: ")
+to_city = input("Enter the arrival city: ")
+departure_time = input("Enter the departure time: ")
+arrival_time = input("Enter the arrival time: ")
 
-# Fetch the result
-result = cursor.fetchall()
+# Initialize variables for your custom logic
+to = to_city
+prev_to = from_city
+result_array = []
 
-# Print the result
-for row in result:
-    print(row)
+while prev_to != to:
+    # Select arrival and to values based on your conditions
+    select_query = """
+    SELECT arrival, `to`
+    FROM your_table_name
+    WHERE departure > %s AND `from` = %s
+    """
 
-# Close the cursor and connection
+    cursor.execute(select_query, (departure_time, prev_to))
+
+    row = cursor.fetchone()
+
+    if row:
+        prev_arrival, prev_to = row
+        result_array.append({"arrival": prev_arrival, "to": prev_to})
+    else:
+        break
+
+# Display the result array
+if result_array:
+    print("Matching travel plans:")
+    for row in result_array:
+        print(row)
+else:
+    print("No matching travel plans found.")
+
+# Close the cursor and connection when done
 cursor.close()
 conn.close()
